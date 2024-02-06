d20630aa89eef7210a7f1d79c65e5107c13f277c
Anand Mohan <andmhn@outlook.com>
2024-02-06 20:43:29 +0530

    correct the tests

--- test/component_test.cpp
+++ test/component_test.cpp
@@ -41,11 +41,39 @@ TEST_CASE("Testing Score functionality")
     auto score = Score();
     CHECK(score.get_points() == 0);
 
-    for (int i = 1; i < 10; i++)
-    {
-        score.add_points(1);
-        CHECK(score.get_points() == i);
-    }
+	//(50, 150, 450, or 900
+	
+	//add 1
+    score.add_points(1);
+    CHECK(score.get_points() == 50);
+
+    score.add_points(1);
+    CHECK(score.get_points() == 100);
+
+	score.add_points(1);
+    CHECK(score.get_points() == 150);
+
+	// add 2
+	score.add_points(2);
+    CHECK(score.get_points() == 300);
+
+	score.add_points(2);
+    CHECK(score.get_points() == 450);
+
+	score.add_points(2);
+    CHECK(score.get_points() == 600);
+	
+	// add 3
+	score.add_points(3);
+    CHECK(score.get_points() == 1050);
+
+	score.add_points(3);
+    CHECK(score.get_points() == 1500);
+
+	// add 4
+	score.add_points(4);
+    CHECK(score.get_points() == 2400);
+
 
     score.reset_score();
     CHECK(score.get_points() == 0);
