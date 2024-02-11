06a33179de7c7270c921d53a82d399b114364290
Anand Mohan <andmhn@outlook.com>
2024-02-12 01:48:38 +0530

    don't check background keys

--- src/instance.cpp
+++ src/instance.cpp
@@ -4,17 +4,23 @@
 
 #include "components.h"
 #include "globals.h"
-#include "tetromino.h"
 #include "grid_entry.h"
+#include "tetromino.h"
 
 /// Globals
 sf::RenderWindow *window;
 sf::Clock clk;
 Score score = Score();
+sf::Event evnt;
 
 // TODO add paused state
-enum Scene {WELCOME, PLAYING, GAME_OVER} scene = WELCOME;
-
+enum Scene
+{
+    WELCOME,
+    PLAYING,
+    GAME_OVER,
+    HELP
+} scene = WELCOME;
 
 /// function declarations
 void draw_border_col(int pos);
@@ -29,13 +35,12 @@ class SceneManager
     sf::Text text;
 
     unsigned int points = 0, rows = 0;
-    bool help_screen = false;
 
     inline void set_heading_text()
     {
         text.setCharacterSize(40);
         text.setFillColor(sf::Color::Yellow);
-        text.setPosition(5 * GRID_SIZE , 5 * GRID_SIZE);
+        text.setPosition(5 * GRID_SIZE, 5 * GRID_SIZE);
     }
 
     inline void set_info_text()
@@ -45,6 +50,7 @@ class SceneManager
         text.setPosition(3 * GRID_SIZE + 10, 10 * GRID_SIZE);
     }
 
+  public:
     void display_help()
     {
         set_info_text();
@@ -52,22 +58,17 @@ class SceneManager
         text.setFillColor(sf::Color::Green);
         text.setCharacterSize(20);
         text.setString(
-"GAME CONTROL:\n\n\n"
-"Left \tmove block left\n\n"
-"Right\tmove block right\n\n"
-"Down \tsoftly land block\n\n\n"
+            "GAME CONTROL:\n\n\n"
+            "Left \tmove block left\n\n"
+            "Right\tmove block right\n\n"
+            "Down \tsoftly land block\n\n\n"
 
-"Ctrl + Right\n\t\tRotate clockwise\n\n"
-"Ctrl + Left\n\t\tRotate anti clockwise\n"
-"\n\n\n\n\n\n(Escape) to exit help"
-        );
+            "Ctrl + Right\n\t\tRotate clockwise\n\n"
+            "Ctrl + Left\n\t\tRotate anti clockwise\n"
+            "\n\n\n\n\n\n(Escape) to exit help");
         window->draw(text);
-
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
-            help_screen = false;
     }
 
-public:
     SceneManager()
     {
         // load font
@@ -87,9 +88,6 @@ public:
 
     void display_welcome()
     {
-        if(help_screen)
-            return display_help();
-
         set_heading_text();
         text.setString("Tetris");
         window->draw(text);
@@ -97,33 +95,26 @@ public:
         set_info_text();
         text.setString("press Enter to play\n\n\n\nF1 -> Tetris Help");
         window->draw(text);
-
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Enter))
-            scene = PLAYING;
-        else if(sf::Keyboard::isKeyPressed(sf::Keyboard::F1))
-            help_screen = true;
     }
 
     void display_game_over()
     {
         set_heading_text();
-        text.setPosition(4 * GRID_SIZE , 5 * GRID_SIZE);
+        text.setPosition(4 * GRID_SIZE, 5 * GRID_SIZE);
         text.setString("Game Over");
         window->draw(text);
 
         set_info_text();
         std::ostringstream os;
-        os << "rows: " << rows << "\t" << "points: " << points
-        <<"\n\n\nPress Enter to replay";
+        os << "rows: " << rows << "\t"
+           << "points: " << points
+           << "\n\n\nPress Enter to replay";
 
         text.setString(os.str());
         window->draw(text);
-
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Enter))
-            scene = PLAYING;
     }
 
-    void play(Tetromino_Controller & controller, Tetromino_Preview & preview)
+    void play(Tetromino_Controller &controller, Tetromino_Preview &preview)
     {
         draw_border_col(SEPERATOR_POS);
 
@@ -139,12 +130,12 @@ public:
         // enable falling blocks
         if (static_cast<unsigned int>(clk.getElapsedTime().asMilliseconds()) > ELAPSED_TIME)
         {
-            if(!controller.has_block_finished())
+            if (!controller.has_block_finished())
                 controller.move(Down);
 
             // spawn new block after time limit
             else
-                 preview.submit_tetromino(controller);
+                preview.submit_tetromino(controller);
 
             clk.restart();
         }
@@ -166,7 +157,6 @@ int instance()
 
     while (window->isOpen())
     {
-        sf::Event evnt;
         while (window->pollEvent(evnt))
         {
             if (evnt.type == sf::Event::Closed)
@@ -177,29 +167,53 @@ int instance()
             {
                 key_handler(evnt.key.code, controller);
             }
+
+            else if (evnt.type == sf::Event::KeyPressed &&
+                     scene == WELCOME)
+            {
+                if (evnt.key.code == sf::Keyboard::Enter)
+                    scene = PLAYING;
+                else if (evnt.key.code == sf::Keyboard::F1)
+                    scene = HELP;
+            }
+            else if (evnt.type == sf::Event::KeyPressed &&
+                     scene == HELP &&
+                     evnt.key.code == sf::Keyboard::Escape)
+            {
+                scene = WELCOME;
+            }
+            else if (evnt.type == sf::Event::KeyPressed &&
+                     scene == GAME_OVER &&
+                     evnt.key.code == sf::Keyboard::Enter)
+            {
+                scene = PLAYING;
+            }
         }
         window->clear();
 
-        if(detect_game_over(preview.preview))
+        if (detect_game_over(preview.preview))
         {
             manager.record_points();
             score.reset();
             Grid_Entry::reset();
-            scene = GAME_OVER;    // set game over scene
+            scene = GAME_OVER; // set game over scene
         }
 
         // Show Different Scenes
-        switch(scene)
+        switch (scene)
         {
-            case WELCOME:
-                manager.display_welcome();
-                break;
-            case PLAYING:
-                manager.play(controller, preview);
-                break;
-            case GAME_OVER:
-                manager.display_game_over();
-                break;
+        case WELCOME:
+            manager.display_welcome();
+            break;
+        case PLAYING:
+            manager.play(controller, preview);
+            break;
+        case GAME_OVER:
+            manager.display_game_over();
+            break;
+        case HELP:
+            manager.display_help();
+            break;
         }
 
         window->display();
@@ -211,15 +225,14 @@ int instance()
 bool detect_game_over(Tetromino tetromino)
 {
     // if there are blocks in starting pos
-    for(auto &b : tetromino.blocks)
+    for (auto &b : tetromino.blocks)
     {
         sf::Vector2i spawning_pos = b.grid_pos;
         spawning_pos.x -= (PREVIEW_X - STARTING_X);
         spawning_pos.y -= PREVIEW_Y;
 
-        if(Grid_Entry::has_entry(spawning_pos))
-             return true;
-
+        if (Grid_Entry::has_entry(spawning_pos))
+            return true;
     }
     return false;
 }
